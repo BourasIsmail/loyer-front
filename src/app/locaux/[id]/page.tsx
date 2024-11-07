798e26d8b416367f5dcfd8afdac8ead2896676b7
ismail <ismailbouras1968@gmail.com>
2024-11-07 14:04:11 +0100

    fix v2

--- src/app/locaux/[id]/page.tsx
+++ src/app/locaux/[id]/page.tsx
@@ -288,11 +288,10 @@ export default function Home({
                           <input
                             type="text"
                             name="contrat"
-                            disabled
                             id=""
                             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                             value={selectedValue?.idContrat || ""}
-                            placeholder="rib"
+                            placeholder="id contrat"
                             onChange={(e) =>
                               setselectedValue({
                                 ...selectedValue,
