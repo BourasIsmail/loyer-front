49e939f38fd8c6a754f35848b3615f98ce576447
ismail <ismailbouras1968@gmail.com>
2024-11-28 08:04:13 +0100

    v5

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -166,21 +166,19 @@ const SideBar = () => {
                 </a>
               </li>
             )}
-            {utilisateur?.roles === "SUPER_ADMIN_ROLES" && (
-              <li>
-                <a
-                  href="/avenant"
-                  className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
-                >
-                  <span className="inline-flex justify-center items-center ml-4">
-                    <MdOutlineNotificationAdd />
-                  </span>
-                  <span className="ml-2 text-sm tracking-wide truncate">
-                    Avenants
-                  </span>
-                </a>
-              </li>
-            )}
+            <li>
+              <a
+                href="/avenant"
+                className="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-indigo-500 pr-6"
+              >
+                <span className="inline-flex justify-center items-center ml-4">
+                  <MdOutlineNotificationAdd />
+                </span>
+                <span className="ml-2 text-sm tracking-wide truncate">
+                  Avenants
+                </span>
+              </a>
+            </li>
             <li>
               <a
                 onClick={logout}
