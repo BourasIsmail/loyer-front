60b7502d349e8c0b49b021047c565bdda3181967
ismail <ismailbouras1968@gmail.com>
2025-02-13 13:21:08 +0100

    ii

--- src/app/proprietaires/[id]/page.tsx
+++ src/app/proprietaires/[id]/page.tsx
@@ -236,7 +236,7 @@ export default function Home({
                   <div className="flex justify-start items-end gap-3">
                     <button
                       type="submit"
-                      disabled={is_Super_Admin}
+                      disabled={!is_Super_Admin}
                       className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
                       Modifier les informations
